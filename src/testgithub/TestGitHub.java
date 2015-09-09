f18756972f0b116fc206f27fc7ab79f96e0cf26d
thomas.frommelt <thomas.frommelt@ntwk206.intern.ritter-online.de>
2015-09-09 16:01:02 +0200

    test 2

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -16,7 +16,7 @@ public class TestGitHub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HI");
+        System.out.println("HIffff");
     }
     
 }
